f6a906cd3b30d1b9ef8d4d2041e7bef582c769c7
polyrider <polyhunt23@gmail.com>
2026-05-11 23:20:32 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -715,7 +715,7 @@ export default function LandingPage() {
         <footer className="landing-footer">
           <div style={{ display:'flex', alignItems:'center', gap:12 }}>
             <span className="footer-copy">© 2025 POLYRIVER</span>
-            <span className="footer-copy" style={{ color: 'var(--accent-yellow)', opacity: 0.8 }}>$POLYRIVER CA: EdwT8zYXJ8XddcJJdo64AZsAGpGBwuThYAn6xcbfbrrr</span>
+            <span className="footer-copy" style={{ color: 'var(--accent-yellow)', opacity: 0.8 }}>$POLYRIVER CA: </span>
           </div>
           <div style={{ display:'flex', alignItems:'center', gap:20 }}>
             <Link href="/about" className="footer-poly">About</Link>
